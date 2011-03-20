389a20cd7128dee7b39f57bfdba82ded803308f9
Jeremy BARTHE <jeremy.barthe@gmail.com>
2011-03-20 13:59:58 +0100

    added jobeet layout

--- src/Acme/JobeetBundle/Controller/DefaultController.php
+++ src/Acme/JobeetBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @extra:Route("/", name="_homepage")
+     * @extra:Template()
+     */
     public function indexAction()
     {
-        return $this->render('AcmeJobeetBundle:Default:index.html.twig');
+        return array();
+        //return $this->render('AcmeJobeetBundle:Default:index.html.twig');
     }
 }
